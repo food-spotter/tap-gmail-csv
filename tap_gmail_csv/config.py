3addf9137eea863123dce10f10d9939a065087cf
Manish Khatri <spotter.food@gmail.com>
2020-04-12 17:53:57 +0100

    Change from `source` to source_type`

--- tap_gmail_csv/config.py
+++ tap_gmail_csv/config.py
@@ -3,36 +3,37 @@ import json
 from tap_gmail_csv.logger import LOGGER as logger
 from voluptuous import Schema, Required, Any, Optional
 
-CONFIG_CONTRACT = Schema({
-    Required('email_address'): str,
-    Required('pickle_base64_encoded'): str,
-    Optional('gmail_label'): str,
-    Required('gmail_search_query'): str,
-    Required('start_date'): str,
-    Required('email_address'): str,
-    Required('tables'): [{
-        Required('name'): str,
-        Required('pattern'): str,
-        Required('key_properties'): [str],
-        Required('format'): Any('csv', 'excel'),
-        Required('source'): Any('attachment', 'url'),
-        Optional('unzip'): bool,
-        Optional('delimiter'): str,
-        Optional('quoting'): Any('QUOTE_MINIMAL', 'QUOTE_ALL', 'QUOTE_NONNUMERIC', 'QUOTE_NONE'),
-        Optional('search_prefix'): str,
-        Optional('field_names'): [str],
-        Optional('worksheet_name'): str,
-        Optional('schema_overrides'): {
-            str: {
-                Required('type'): Any(str, [str]),
-                Required('_conversion_type'): Any('string',
-                                                  'integer',
-                                                  'number',
-                                                  'date-time')
+CONFIG_CONTRACT = Schema(
+    {
+        Required("email_address"): str,
+        Required("pickle_base64_encoded"): str,
+        Optional("gmail_label"): str,
+        Required("gmail_search_query"): str,
+        Required("start_date"): str,
+        Required("email_address"): str,
+        Required("tables"): [
+            {
+                Required("name"): str,
+                Required("pattern"): str,
+                Required("key_properties"): [str],
+                Required("format"): Any("csv", "excel"),
+                Required("source_type"): Any("attachment", "url"),
+                Optional("unzip"): bool,
+                Optional("delimiter"): str,
+                Optional("quoting"): Any("QUOTE_MINIMAL", "QUOTE_ALL", "QUOTE_NONNUMERIC", "QUOTE_NONE"),
+                Optional("search_prefix"): str,
+                Optional("field_names"): [str],
+                Optional("worksheet_name"): str,
+                Optional("schema_overrides"): {
+                    str: {
+                        Required("type"): Any(str, [str]),
+                        Required("_conversion_type"): Any("string", "integer", "number", "date-time"),
+                    }
+                },
             }
-        }
-    }]
-})
+        ],
+    }
+)
 
 
 def load(filename):
@@ -41,7 +42,7 @@ def load(filename):
     try:
         with open(filename) as handle:
             config = json.load(handle)
-    except:
+    except Exception:
         logger.fatal("Failed to decode config file. Is it valid json?")
         raise RuntimeError
 
